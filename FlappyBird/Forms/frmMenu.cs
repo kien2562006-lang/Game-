b6f9f135a2e0eda0e6e5af81aefd86ee6aae0d7c
Bui Trung Kien <emailcuaban@example.com>
2026-04-22 16:21:34 +0700

    Tuatdoan

--- FlappyBird/Forms/frmMenu.cs
+++ FlappyBird/Forms/frmMenu.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace FlappyBird
+namespace FlappyBird.Forms
 {
-    public partial class Form1 : Form
+    public partial class frmMenu : Form
     {
-        public Form1()
+        public frmMenu()
         {
             InitializeComponent();
         }
