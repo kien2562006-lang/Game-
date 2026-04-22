ee44d73f429ffcef9fc27876fb9033ba01e24b73
Bui Trung Kien <emailcuaban@example.com>
2026-04-22 22:15:43 +0700

    Tuat

--- FlappyBird/Classes/MapConfig.cs
+++ FlappyBird/Classes/MapConfig.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 
 namespace FlappyBird.Classes
 {
-    internal class MapConfig
-    {
-    }
+
+}
+
+public enum MapType
+{
+    Forest,
+    Volcano,
+    Space,
+    Storm
+}
+
+public class MapConfig
+{
+    //MAP 
+    public MapType Type { get; set; }
+
+ 
+    public int PipeSpeed { get; set; }      // Tốc độ cột di chuyển
+    public int GapSize { get; set; }        // Khoảng hở giữa 2 cột
+    public int Gravity { get; set; }        // Trọng lực map (Space thấp hơn)
+    public bool WindActive { get; set; }    // Có gió thổi ngược không
+
+    // ── SPAWN ──────────────────────────────────
+    public int EnemySpawnScore { get; set; } // Điểm bắt đầu xuất hiện quạ
+
+    // ── HÌNH ẢNH ──────────────────────────────
+    public Image Background { get; set; }   // Ảnh nền map
+    public Image PipeImage { get; set; }    // Ảnh cột theo map
 }
